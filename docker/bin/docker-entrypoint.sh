f5cfb6e84005f29d9c6657082d75253a3dc7da84
Domenico DiNicola <dom.dinicola@gmail.com>
2026-02-06 10:51:03 +0100

    feature ci

--- docker/bin/docker-entrypoint.sh
+++ docker/bin/docker-entrypoint.sh
@@ -1,43 +1,48 @@
-#!/bin/bash
-set -e
-export NGINX_MAX_BODY_SIZE="${NGINX_MAX_BODY_SIZE:-30M}"
-export NGINX_CACHE_DIR="${NGINX_CACHE_DIR:-/data/nginx/cache}"
-export MEDIA_ROOT="${MEDIA_ROOT:-/var/media}"
-export STATIC_ROOT="${STATIC_ROOT:-/var/static}"
+#!/bin/sh -e
 
-export REDIS_LOGLEVEL="${REDIS_LOGLEVEL:-warning}"
-export REDIS_MAXMEMORY="${REDIS_MAXMEMORY:-100Mb}"
-export REDIS_MAXMEMORY_POLICY="${REDIS_MAXMEMORY_POLICY:-volatile-ttl}"
 
-export DOLLAR='$'
+export MEDIA_ROOT="${MEDIA_ROOT:-/var/run/app/media}"
+export STATIC_ROOT="${STATIC_ROOT:-/var/run/app/static}"
+export UWSGI_PROCESSES="${UWSGI_PROCESSES:-"4"}"
+export DJANGO_SETTINGS_MODULE="hope_documents.config.settings"
 
-mkdir -p /var/run /var/nginx ${NGINX_CACHE_DIR} ${MEDIA_ROOT} ${STATIC_ROOT}
-echo "created support dirs /var/run '${MEDIA_ROOT}' '${STATIC_ROOT}' "
-echo "Startup command is: '$1'"
+chown -R hope:unicef /var
 
 case "$1" in
-    "run")
-        envsubst < /conf/nginx.conf.tpl > /conf/nginx.conf && /usr/sbin/nginx -tc /conf/nginx.conf
-
-        django-admin upgrade --no-input
-
-        /usr/sbin/nginx -c /conf/nginx.conf
-        exec uwsgi --ini /conf/uwsgi.ini
-
-    ;;
-    "dev")
-        until pg_isready -h db -p 5432;
-          do echo "waiting for database"; sleep 2; done;
-        django-admin collectstatic --no-input
-        django-admin migrate
-        django-admin runserver 0.0.0.0:8000
-    ;;
-    "setup")
-        until pg_isready -h db -p 5432;
-          do echo "waiting for database"; sleep 2; done;
-        django-admin upgrade --no-input
-    ;;
-*)
-exec "$@"
-;;
+    run)
+      django-admin upgrade --with-check
+	    set -- tini -- "$@"
+	    MAPPING=""
+	    if [ "${STATIC_URL}" = "/static/" ]; then
+	      MAPPING="--static-map ${STATIC_URL}=${STATIC_ROOT}"
+	    fi
+      set -- tini -- "$@"
+	    set -- uwsgi --http :8000 \
+	          -H /venv \
+	          --module hope_documents.config.wsgi \
+	          --mimefile=/conf/mime.types \
+	          --uid hope \
+	          --gid unicef \
+            --buffer-size 8192 \
+            --http-buffer-size 8192 \
+	          $MAPPING
+	    ;;
+    upgrade)
+      django-admin upgrade --with-check
+      ;;
+    worker)
+      set -- tini -- "$@"
+      set -- gosu hope:unicef celery -A hope_documents.config.celery worker --statedb /var/worker --concurrency=4 -E --loglevel=ERROR
+      ;;
+    beat)
+      set -- tini -- "$@"
+      set -- gosu hope:unicef celery -A hope_documents.config.celery beat --loglevel=ERROR --scheduler django_celery_beat.schedulers:DatabaseScheduler
+      ;;
+    flower)
+      export DATABASE_URL="sqlite://:memory:"
+      set -- tini -- "$@"
+      set -- gosu hope:unicef celery -A hope_documents.config.celery flower
+      ;;
 esac
+
+exec "$@"
